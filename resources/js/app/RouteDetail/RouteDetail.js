8df850d35f913cf3f2068d105528b4796f616bb0
Šimon Cipro <simon.cipro@gmail.com>
2020-11-20 21:55:29 +0100

    route detail showing basic informations about route and the map

--- resources/js/app/RouteDetail/RouteDetail.js
+++ resources/js/app/RouteDetail/RouteDetail.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect} from 'react'
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import DisplayMapWithRoute from '../DisplayMapWithRoute/DisplayMapWithRoute';
 
 const RouteDetail = () => {
     // get the id from url to fetch specific route and its relations
@@ -23,6 +24,9 @@ const RouteDetail = () => {
         fetchData();
     }, []);
 
+    const centerCoordinates = [15.192371159791946, 50.75322702527046],
+        zoom = 15;
+
     return (
         <div className="route-detail-container">
         {
@@ -30,7 +34,22 @@ const RouteDetail = () => {
                 (
                     <h3>Loading</h3>
                 ) : (
-                    <div className="route-name">{ data.route.name }</div>
+                    <>
+                    
+                    <div className="route-data-container">
+                        <div className="route-data__name">{ data.route.name }</div>
+                        <div className="route-data">
+                            <div className="route-data__item">Total elevation: { data.route.elevation_gain } m</div>
+                            <div className="route-data__item">Distance: { data.route.length / 1000 } km</div>
+                            <div className="route-data__item">Difficulty: { data.route.difficulty } / 5</div>
+                            <div className="route-data__item">Ratings: TO DO %</div>
+                        </div>
+                    </div>
+
+                    <div className="map-container">
+                        <DisplayMapWithRoute zoom={zoom} url={'/storage/gpx/' + data.route.url} centerCoordinates={centerCoordinates} />
+                    </div>
+                    </>
                 )
         }
       </div>
