ff5ab28231c15c3147847b56e602442544aeba1b
Šimon Cipro <simon.cipro@gmail.com>
2020-11-22 00:48:05 +0100

    map with routes clickable w link to specific route

--- resources/js/app/DisplayMapWithPoints/DisplayMapWithPoints.js
+++ resources/js/app/DisplayMapWithPoints/DisplayMapWithPoints.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import 'ol/ol.css';
 import GPX from 'ol/format/GPX';
 import Map from 'ol/Map';
@@ -8,12 +8,14 @@ import XYZ from 'ol/source/XYZ';
 import {Circle as CircleStyle, Fill, Stroke, Style, Icon} from 'ol/style';
 import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer';
 import {fromLonLat} from 'ol/proj';
-import Attribution from 'ol/control/Attribution';
+// import Attribution from 'ol/control/Attribution';
 import Feature from 'ol/Feature';
 import Point from 'ol/geom/Point';
+import Overlay from 'ol/Overlay';
+import { Link } from 'react-router-dom';
 
 const DisplayMapWithPoints = (props) => {
-    
+    const [selectedRoute, setSelectedRoute] = useState([]);
 
     useEffect(() => {
         
@@ -47,6 +49,7 @@ const DisplayMapWithPoints = (props) => {
                     name: route.name,
                     length: route.length,
                     elev: route.elevation_gain,
+                    id: route.id,
                 });
                 
                 const iconStyle = new Style({
@@ -70,11 +73,56 @@ const DisplayMapWithPoints = (props) => {
     
                 mapObject.addLayer(vectorLayer);
             });
-    }, [])
+            
+            mapObject.on('click', function (event) {
+                const feature = mapObject.forEachFeatureAtPixel(event.pixel, function (feature) {
+                  return feature;
+                });
+                if (feature) {
+                  const coordinates = feature.getGeometry().getCoordinates();
+                  
+                  console.log(feature.values_.name, coordinates);
+
+                  const element = document.getElementById('popup');
+                //   element.innerHTML = `${feature.values_.name}`;
+
+                  setSelectedRoute([feature.values_.name, feature.values_.elev, feature.values_.id]);
+                  
+                  const popup = new Overlay({
+                    element: element,
+                    autoPan: true,
+                    autoPanAnimation: {
+                        duration: 250,
+                    },
+                  });
+
+                  popup.setPosition(coordinates);
+                  mapObject.addOverlay(popup);
+
+                } else {
+                  console.log('hovno');
+                }
+              });
+        
+        
+    }, []);
 
 
     return (
-     <div id="map" className="map"></div>
+     <div id="map" className="map">
+         <div id="popup" className="popup">
+         {
+             selectedRoute !== [] && (
+                    <>
+                    <Link to={'/route/' + selectedRoute[2] }> { selectedRoute[0] } </Link>
+                    <div className="route-elev">Route elev: { selectedRoute[1]} m</div> 
+                    </>
+              ) 
+                 
+             
+         }
+         </div>
+     </div>
     )
 }
 
