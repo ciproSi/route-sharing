504f0f17da5e7403279a76450f2bd879831e9b25
Šimon Cipro <simon.cipro@gmail.com>
2020-11-23 12:10:18 +0100

    redirection after succesfull login, login form protection from being displayed for logged in users

--- resources/js/app/Login/Login.jsx
+++ resources/js/app/Login/Login.jsx
@@ -1,59 +1,76 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useContext, useEffect } from 'react';
+import { Redirect } from 'react-router-dom';
 import ApiClient from '../ApiClient';
+import { UserContext } from '../App/App.jsx';
 
 export default function Login({ fetchUser }) {
-
-   const [values, setValues] = useState({
+    const user = useContext(UserContext);
+    
+    const [redirect, setRedirect] = useState(null);
+    const [values, setValues] = useState({
       email: '',
       password: '',
-  }); 
+    }); 
 
-  const handleChange = (event) => {
-   const allowed_names = ['email', 'password'],
-       name = event.target.name,
-       value = event.target.value
+    const handleChange = (event) => {
+        const allowed_names = ['email', 'password'],
+            name = event.target.name,
+            value = event.target.value
 
-   if (-1 !== allowed_names.indexOf(name)) {
-       setValues(prev_values => {
-           return({
-               ...prev_values,
-               [name]:value    
-           });
-       });
-   }
-}
+        if (-1 !== allowed_names.indexOf(name)) {
+            setValues(prev_values => {
+                return({
+                    ...prev_values,
+                    [name]:value    
+                });
+            });
+        }
+    }
 
-const handleSubmit = async (event) => {
-   event.preventDefault();
+    const handleSubmit = async (event) => {
+        event.preventDefault();
 
-   const response = await ApiClient.post('/login', {
-      body: JSON.stringify(values)
-  });
+        const response = await ApiClient.post('/login', {
+            body: JSON.stringify(values)
+        });
 
-  if (response.status === 200) {
-      // we are logged in!
-      fetchUser(); // tell App to fetch the user again
-  } else {
-      // display an error message?
-  }
-}
+        if (response.status === 200) {
+            // we are logged in!
+            fetchUser(); // tell App to fetch the user again
+            setRedirect('/profile');
+        } else {
+            // display an error message?
+        }
+    }
 
+    if (redirect) {
+        return (
+            <Redirect to={ redirect } />
+        )
+    }
+    return(
+        <div>
+            <h1>Login</h1>
+            {
+                user == null ? (
+                    <form action="/login" method="post" className="login-form" onSubmit={ handleSubmit }>
+                        <div className="form_group">
+                            <label htmlFor="email">Email</label>
+                            <input type="email" name="email"  value={ values.email } onChange={ handleChange }/>
+                        </div>
 
-   return(
-      <div>
-         <h1>Login</h1>
-         <form action="/login" method="post" className="login-form" onSubmit={ handleSubmit }>
-            <div className="form_group">
-                  <label htmlFor="email">Email</label>
-                  <input type="email" name="email"  value={ values.email } onChange={ handleChange }/>
-            </div>
-
-            <div className="form_group">
-                  <label htmlFor="password">Password</label>
-                  <input type="password" name="password" value={ values.password } onChange={ handleChange } />
-            </div>
-            <button>Log in</button>
-         </form>
-      </div> 
-   );
+                        <div className="form_group">
+                            <label htmlFor="password">Password</label>
+                            <input type="password" name="password" value={ values.password } onChange={ handleChange } />
+                        </div>
+                        <button>Log in</button>
+                    </form>
+                ) : (
+                    <h3>You are already logged in.</h3>
+                )
+            }
+            
+            
+        </div> 
+    );
 }
