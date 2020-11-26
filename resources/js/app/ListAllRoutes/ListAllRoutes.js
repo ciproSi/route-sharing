43625c831f79704d5b2171ff8620709276cd7c13
radamusil <64097743+radamusil@users.noreply.github.com>
2020-11-26 17:20:35 +0100

    route cards

--- resources/js/app/ListAllRoutes/ListAllRoutes.js
+++ resources/js/app/ListAllRoutes/ListAllRoutes.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import RouteBox from '../RouteBox/RouteBox';
 
 const ListAllRoutes = (props) => {
         return (
             <div className="route-list">
                 {
                     props.routes.map((route, index) => (
+
+                        <RouteBox key={ index } route={ route } />
                         
-                        <div key={ index } className="route-list__name">
+/*                         <div key={ index } className="route-list__name">
                             <Link to={'/route/' + route.id }> { route.name } </Link>
-                        </div> 
+                        </div>  */
                     ))
 
                 }
